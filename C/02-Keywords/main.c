f9bbffe240a5ceea94097fd3dfbcfc53eb53287a
Meet Chopra <meetchopra222@gmail.com>
2026-01-22 01:17:23 +0530

    info on keywords added

---
+++ C/02-Keywords/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+
+    /*
+    Keywords are predefined or reserved words which have special meanings to the compiler,
+    these are part of syntax and cannot be used as identifiers.
+    
+    there are 32 0f these in c 
+
+    here is a example code which will produce an error because we used return as identifier  
+    */
+/*
+    int return = 10;   
+    printf("%d\n", return);
+    return 0;
+*/
+    }
