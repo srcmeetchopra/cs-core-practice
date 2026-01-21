2db69060b0622c503c067a3f14789888f0d53d2f
Meet Chopra <meetchopra222@gmail.com>
2026-01-22 00:01:10 +0530

    Delete Java-lab directory

--- Java-lab/java/helloworld.java
+++
@@ -1,8 +0,0 @@
-class helloworld{  // class name (file name)
-    public static void main(String[] args) {
-
-        System.out.println("This is inside the block.");
-        System.out.println("This is outside the block.");
-        System.out.println("This is outside the block.");
-    }
-}
