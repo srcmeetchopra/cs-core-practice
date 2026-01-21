3d8fec1ecc084caeff991366259e5a2a64b816f8
Meet Chopra <meetchopra222@gmail.com>
2026-01-21 23:51:41 +0530

    first commit

---
+++ Java-lab/java/helloworld.java
@@ -0,0 +1,8 @@
+class helloworld{  // class name (file name)
+    public static void main(String[] args) {
+
+        System.out.println("This is inside the block.");
+        System.out.println("This is outside the block.");
+        System.out.println("This is outside the block.");
+    }
+}
