6ec22601e1a63c6b327343b13e85c6802e7c9659
Meet Chopra <meetchopra222@gmail.com>
2026-01-22 00:49:21 +0530

    01-identifiers

---
+++ C/01-Identifiers/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+    
+    //identifiers are user defined which a user can use to identify variables, functions, arrays, structures.
+    
+    int variable;
+    variable = 10;
+    printf("Variavble is a Identifier Who's value is %d");
+
+}
