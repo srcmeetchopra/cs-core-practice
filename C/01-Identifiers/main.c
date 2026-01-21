a88755069e32677f495b29955f717b7e427e74ba
Meet Chopra <meetchopra222@gmail.com>
2026-01-22 01:08:31 +0530

    added info

--- C/01-Identifiers/main.c
+++ C/01-Identifiers/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 int main (){
+    /*
+links - Read this for more info "https://www.geeksforgeeks.org/c/c-identifiers/" 
+
+    identifiers are user defined which a user can use to identify variables, functions, arrays, structures.
+     identifiers can only contain alphabets, digits(0-9), underscore(_)
     
-    //identifiers are user defined which a user can use to identify variables, functions, arrays, structures.
-    
-    int variable;
+     Rules :-
+     first charater of an identifier must be a letter or an underscore.
+   */
+   int variable;
     variable = 10;
     printf("Variavble is a Identifier Who's value is %d");
 
