15b74ee882909edf835645cd8c25588ed0e81a33
Meet Chopra <meetchopra222@gmail.com>
2026-01-22 01:35:32 +0530

    info on Variables

---
+++ C/03-Variables/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+
+    /*
+    A variable in C is a named piece of memory 
+    which is used to store data and access it whenever required.
+
+    there are different data types for storring almost evey kind of data eg int float  char etc*/
+    int val1 = 10;
+    float val2 = 120022.125;
+    char ch = 'a';
+
+    // now if we want to print it we have to use datatypes specifiers eg %d, %f, %fl, %c
+
+    printf("These are the values we stored preveously:\n int = %d\n float = %f\n char = %c\n", val1, val2, ch);
+
+}
